839ef936aac537cb04c30658aa1decc6b61cfd8d
Pal Kerecsenyi <pal@p.kerecs.com>
2024-03-05 14:06:29 +0000

    Ensure all transactions are serializable

--- packages/backend/src/database/client.ts
+++ packages/backend/src/database/client.ts
@@ -1,69 +1,76 @@
-import {PrismaClient} from "./generated-models/index.js";
-import {TransactionType} from "../types/prisma.js";
+import { PrismaClient } from "./generated-models/index.js";
+import { TransactionType } from "../types/prisma.js";
 import { PrismaClientKnownRequestError } from "./generated-models/runtime/library.js";
 
 class TransactionInterruptError extends Error {
-    constructor() {
-        super("TRANSACTION_INTERRUPT");
-    }
+	constructor() {
+		super("TRANSACTION_INTERRUPT");
+	}
 }
 
 export interface InterruptibleTransaction extends TransactionType {
-    rollback(): void
+	rollback(): void;
 }
 
 export class DBClient {
-    private static client: PrismaClient = new PrismaClient()
-    private static disconnected = false
-    static getClient() {
-        if (this.disconnected) {
-            throw new Error("tried to use Prisma client after disconnect")
-        }
-        return this.client
-    }
-    static async disconnect() {
-        if (this.disconnected) {
-            throw new Error("tried to disconnect already disconnected Prisma client")
-        }
-        await this.client.$disconnect()
-        this.disconnected = true
-    }
-    static async init() {
-        return this.client.$connect()
-    }
+	private static client: PrismaClient = new PrismaClient();
+	private static disconnected = false;
+	static getClient() {
+		if (this.disconnected) {
+			throw new Error("tried to use Prisma client after disconnect");
+		}
+		return this.client;
+	}
+	static async disconnect() {
+		if (this.disconnected) {
+			throw new Error("tried to disconnect already disconnected Prisma client");
+		}
+		await this.client.$disconnect();
+		this.disconnected = true;
+	}
+	static async init() {
+		return this.client.$connect();
+	}
 
-    static async interruptibleTransaction<T>(callback: (tx: InterruptibleTransaction) => Promise<T>) {
-        const client = DBClient.getClient()
-        try {
-            return await client.$transaction(async tx => {
-                return callback({
-                    ...tx,
-                    rollback() {
-                        throw new TransactionInterruptError()
-                    }
-                })
-            })
-        } catch (e) {
-            if (e instanceof TransactionInterruptError) {
-                return undefined
-            }
-            throw e
-        }
-    }
+	static async interruptibleTransaction<T>(
+		callback: (tx: InterruptibleTransaction) => Promise<T>,
+	) {
+		const client = DBClient.getClient();
+		try {
+			return await client.$transaction(
+				async (tx) => {
+					return callback({
+						...tx,
+						rollback() {
+							throw new TransactionInterruptError();
+						},
+					});
+				},
+				{
+					isolationLevel: "Serializable",
+				},
+			);
+		} catch (e) {
+			if (e instanceof TransactionInterruptError) {
+				return undefined;
+			}
+			throw e;
+		}
+	}
 
-    static generateErrorMessage(error: any): string {
-        if (error instanceof PrismaClientKnownRequestError) {
-            switch (error.code) {
-                case "P2002":
-                    return `${error.meta!["field_name"]} already exists`
-                case "P2003":
-                    return `${error.meta!["field_name"]} not found`
-                case "P2025":
-                    return `Couldn't find resource to assign`
-            }
-        }
+	static generateErrorMessage(error: any): string {
+		if (error instanceof PrismaClientKnownRequestError) {
+			switch (error.code) {
+				case "P2002":
+					return `${error.meta!["field_name"]} already exists`;
+				case "P2003":
+					return `${error.meta!["field_name"]} not found`;
+				case "P2025":
+					return `Couldn't find resource to assign`;
+			}
+		}
 
-        console.warn(error)
-        return "Something went wrong"
-    }
+		console.warn(error);
+		return "Something went wrong";
+	}
 }
